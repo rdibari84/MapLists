095447f977c4e3ce8a3dad84cf81ca3a92c2b7f5
Rebecca DiBari <rdibari1@gmail.com>
2018-03-14 17:43:52 +0100

    using promise in get storage, saving apikey

--- background.js
+++ background.js
@@ -1,58 +1,52 @@
-
-// called when Chrome starts
-chrome.runtime.onStartup.addListener(function () {
-    get_apikey().then( function(value) {
-	send_message(value);
-    });
-});
-
 //Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-    console.log("heard browser action");
-    send_message( "browser_action_clicked");
-    get_apikey().then( function(value) {
-        send_message(value);
-    });
+  console.log("heard browser action");
+  objToSend = {}
+  objToSend["message"]="firstTime";
+  get_apikey().then( function(value) {
+    objToSend["apikey"]=value;
+    send_message(objToSend);
+  });
 });
 
 // needed to display color when navigation to/from the web page occurs after first navigation has occured.
 chrome.history.onVisited.addListener(function onVisited(tab) {
-    console.log("onVisited!");
-    send_message("history_on_visited");
+  console.log("onVisited!");
+  chrome.tabs.executeScript(null,{file:"content.js"});
 });
 
-// needed to load the color on first naviagtion to the page
-// TODO: understand why just sending a message doesn't work
+//needed to load the color on first naviagtion to the page
+//TODO: understand why just sending a message doesn't work
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-    chrome.tabs.executeScript(null,{file:"content.js"});
+  chrome.tabs.executeScript(null,{file:"content.js"});
 });
 
 function send_message(value){
-    // Send a message to the active tab                                                                                                                                                                                                  
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, {"message": value});
-	console.log("sent message: ", value, "!");
-    });
+  // Send a message to the active tab
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {"message": value});
+    console.log("sent message: ", value, "!");
+  });
 }
 
 function get(url) {
   return new Promise(function(resolve, reject) {
-      var req = new XMLHttpRequest();
-      req.open('GET', url, true);
-      req.onload = function() {
-	  if (req.status == 200) {
-              console.log("loaded the file");
-              resolve(req.response);
-	  }
-	  else {
-              reject(Error(req.statusText));
-	  }
-      };
-      req.onerror = function() {
-	  reject(Error("Network Error"));
-      };
-      req.send();
+    var req = new XMLHttpRequest();
+    req.open('GET', url, true);
+    req.onload = function() {
+      if (req.status == 200) {
+        console.log("sucessfully loaded the file: ", url);
+        resolve(req.response);
+      }
+      else {
+        reject(Error(req.statusText));
+      }
+    };
+    req.onerror = function() {
+      reject(Error("Network Error"));
+    };
+    req.send();
   }); // close return promise;
 }
 
@@ -61,10 +55,10 @@ function getJSON(file) {
 }
 
 function get_apikey(){
-    return getJSON("config.json").then( function(config) {
-	return config["API_KEY"];
-    }, function(err) {
-    	console.log("error: ", err);
-	reject(err);
-    })
+  return getJSON("config.json").then( function(config) {
+    return config["API_KEY"];
+  }, function(err) {
+    console.log("error: ", err);
+    reject(err);
+  })
 }
--- content.js
+++ content.js
@@ -1,70 +1,97 @@
 
-var key = "cafeColor";
-var apikey;
+var colorkey = "cafeColor";
+var apikey = "apikey";
+var colorToSet = "red";
 
-chrome.extension.onMessage.addListener(
-    function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(
+  function(request) {
 
-	if (request.message.startsWith("A")) {
-	    apikey = request.message;
-	}
+    // if this is the first time the extension has been loaded
+    if (request.message["message"] == "firstTime") {
+      console.log("firstTime!");
+      var mylist = accessListElements();
+      setListColor(mylist, colorToSet);
+      saveInfo(colorkey, colorToSet);
+      // see if apikey has been saved
+      getStorage(apikey).then( function(value) {
+        console.log(apikey, value);
+        if (value == null) {
+            value = request.message["apikey"];
+            saveInfo(apikey, value);
+            loadScript("https://maps.googleapis.com/maps/api/js?key="+value+"&callback=initMap");
+        }
+      });
+    } // firstTime
 
-	if (request.message == "browser_action_clicked") {
-	    console.log("browser_action_clicked");
-	    var mylist = accessListElements();
-	    setListColor(mylist, "red", true);
-	} // close browser action
-
-	if (request.message == "history_on_visited") { 
-	    console.log("checking if there is a saved color");
-	    chrome.storage.sync.get("cafeColor", function(result) {
-		console.log("color: ", result[key]);
-		if (result.key != 'undefined') {
-		    var mylist = accessListElements();
-		    setListColor(mylist, result[key], false);
-		} else {
-		    console.log("color is undefined");
-		}
-	    });
-	} // close history
-    } // close function
+  }// close function
 ); // close chrome extension
 
-function setListColor(mylist, color, saveObject){
-    mylist.forEach( function(currentValue) { 
-	element = currentValue;
-	textElement = element.querySelector("div.section-list-item-text");
-	icon = element.querySelector("div.section-common-icon");
-	
-	if (textElement.textContent.includes('cafe')) {
-	    icon.style.backgroundColor = color;
-	    if (saveObject) {
-		save_options("cafeColor", color);
-	    }
-	}
-    });
+getStorage(colorkey).then( function(value) {
+  var mylist = accessListElements();
+  setListColor(mylist, value);
+});
+
+
+///////////////// Helper Methods ///////////////////////////
+
+function getStorage(key){
+  return new Promise(function(resolve, reject) {
+    chrome.storage.sync.get(key, function(result) {
+      console.log(key, result[key]);
+      resolve(result[key]);
+    }); // close chrome.storage.sync.get
+  }); // new Promise
+}
+
+function loadScript(url){
+  console.log("loading script. url: ", url);
+  var script = document.createElement("script");
+  script.src = url;
+  script.defer = true;
+  document.body.appendChild(script);
+}
+
+function initMap() {
+  bounds = new google.maps.Map.getBounds();
+  console.log("map bounds: {}", bounds)
+  // map = new google.maps.Map(document.getElementById('map'), {
+  //     center: {
+  //         lat: -34.397,
+  //         lng: 150.644
+  //     },
+  //     zoom: 10
+  // });
+}
+
+function setListColor(mylist, color){
+  mylist.forEach( function(currentValue) {
+    element = currentValue;
+    textElement = element.querySelector("div.section-list-item-text");
+    icon = element.querySelector("div.section-common-icon");
+
+    if (textElement.textContent.includes('cafe')) {
+      icon.style.backgroundColor = color;
+    }
+  });
 }
 
 function accessListElements() {
-    var singleElement = document.querySelectorAll("div.section-common-icon");
-    var savedPlacesList = document.querySelectorAll("button.section-list-item-content.section-list-item-button");
-    return savedPlacesList;
+  var singleElement = document.querySelectorAll("div.section-common-icon");
+  var savedPlacesList = document.querySelectorAll("button.section-list-item-content.section-list-item-button");
+  return savedPlacesList;
 }
 
-function save_options(key, value) {
-    var obj = {}
-    obj[key]=value;
-    chrome.storage.sync.set(obj, function(){
-        console.log("saving data: ", obj);
-    });
+function saveInfo(key, value) {
+  var obj = {}
+  obj[key]=value;
+  chrome.storage.sync.set(obj, function(){
+    console.log("saving ", key);
+  });
 
-    // make sure its saved
-    chrome.storage.sync.get(key, function(result) {
-	var color = result[key];
-	if (color == 'undefined') {
-	    // throw error
-	}
-    });
+  // make sure its saved
+  chrome.storage.sync.get(key, function(result) {
+    var value = result[key];
+  });
 }
 
 // <div class="dropdown">
