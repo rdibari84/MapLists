ef282285864a679c6ecc781beb7647b9d2aa4a66
Rebecca DiBari <rdibari1@gmail.com>
2018-03-26 21:26:52 +0200

    glued new select color button to icon using id

--- content.js
+++ content.js
@@ -1,6 +1,13 @@
-var colorkey = "cafeColor";
+/**
+Important Note!!
+- The name of the list is set as the id of the dropdown div.
+- This id will be used as the storageKey when a color is selected (This links the button back to the icon)
+- When looking to see which color to display for which list, the app will loop through all saved keys
+and if it finds a list with that name, will color it
+**/
 var apikey = "apikey";
-var colorToSet = "red";
+
+chrome.storage.sync.clear();
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
@@ -15,6 +22,7 @@ chrome.runtime.onMessage.addListener(
       sendResponse("recieved message");
     } // firstTime
 
+    getAllStorageKeys();
     addSelectColorButton();
     addSelectColorButtonFunctionality();
 
@@ -23,25 +31,203 @@ chrome.runtime.onMessage.addListener(
 
     if (request.message["message"] == "updated") {
       console.log("updated!");
-      getStorage(colorkey).then( function(value) {
-        var mylist = accessListElements();
-        setListColor(mylist, value);
-      });
+      getAllStorageKeys();
       sendResponse("recieved message");
     }
   }// close function
 ); // close chrome extension
 
+//////////////////////////////////////////////////////
 ///////////////// Helper Methods ///////////////////////////
+//////////////////////////////////////////////////////
 
-function getStorage(key){
+/*
+ chrome storage
+ */
+ function getStorage(key){
+   return new Promise(function(resolve, reject) {
+     chrome.storage.sync.get(key, function(result) {
+       resolve(result[key]);
+     }); // close chrome.storage.sync.get
+   }); // new Promise
+ }
+
+function getAllStorageKeys(){
   return new Promise(function(resolve, reject) {
-    chrome.storage.sync.get(key, function(result) {
-      resolve(result[key]);
+    chrome.storage.sync.get(null, function(result) {
+      for (key in result) {
+        console.log("stored key: ", key);
+        setColor(key, result[key])
+      }
     }); // close chrome.storage.sync.get
   }); // new Promise
 }
 
+function saveStorage(key, value) {
+  var obj = {}
+  obj[key]=value;
+  chrome.storage.sync.set(obj, function(){
+    console.log("saving ", key);
+  });
+
+  // make sure its saved
+  chrome.storage.sync.get(key, function(result) {
+    var value = result[key];
+  });
+}
+
+function setColor(listName, color){
+  var mylist = accessListElements();
+  mylist.forEach( function(currentValue) {
+    element = currentValue;
+    textElement = element.querySelector("div.section-list-item-text");
+    icon = element.querySelector("div.section-common-icon");
+    if (textElement.textContent.includes(listName)) {
+      console.log("text contains ", listName);
+      icon.style.backgroundColor = color;
+    }
+  });
+}
+
+function setColorOfElement(fullElement, color){
+    console.log("fullElement ", fullElement);
+    icon = element.querySelector("div.section-common-icon");
+    icon.style.backgroundColor = color;
+}
+
+
+function accessListElements() {
+  var savedPlacesList = document.querySelectorAll("button.section-list-item-content.section-list-item-button");
+  return savedPlacesList;
+}
+
+//////////////////////// html additions ////////////////////
+
+/*
+ Select Color Button
+ Important!! set the Name of the list as the dropdown id. this is the glue links the button back to the icon
+ */
+function addSelectColorButton() {
+  var threeColons = document.querySelectorAll('div.maps-sprite-common-more.section-list-item-secondary-content');
+  for (var i = 0; i < threeColons.length; i++) {
+      var myelem = threeColons[i];
+      console.log("myelem: ", myelem);
+      var nameOfList = myelem.parentNode.parentNode.querySelector("div.section-list-item-text").textContent.trim().split(" ")[0];
+      //console.log("nameOfList: ", nameOfList);
+      $(myelem).click(function() {
+          console.log("myelem1: ", myelem);
+          console.log("nameOfList1: ", nameOfList);
+          var contextMenu = document.getElementById("context-menu");
+          var existingElement = document.getElementById("newOutterDiv");
+          if (contextMenu != null && existingElement == null) {
+            // outter most div
+            console.log("adding select color button");
+            var d = document.createElement('div');
+            d.id = nameOfList;
+            d.className = "context-menu-entry kd-menulistitem dropright show";
+            contextMenu.appendChild(d);
+
+            // Select Color Button
+            var b = document.createElement('button');
+            b.id = nameOfList + "Button";
+            b.className = "context-menu-entry-text dropdown-toggle";
+            b.setAttribute("data-toggle", "dropdown");
+            b.innerText = "Select Color";
+            d.appendChild(b);
+
+            // the dropdown content
+            var dropdownDiv = document.createElement('div');
+            dropdownDiv.className="dropdown-menu";
+            dropdownDiv.setAttribute("aria-labelledby","dropdownMenuLink");
+            d.appendChild(dropdownDiv);
+
+            var item1 = document.createElement('a');
+            item1.className = "dropdown-item";
+            item1.id = "redColor";
+            item1.href = "#";
+            item1.innerText = "Red";
+            dropdownDiv.appendChild(item1);
+
+            var item2 = document.createElement('a');
+            item2.className = "dropdown-item";
+            item2.id = "blueColor";
+            item2.href = "#";
+            item2.innerText = "Blue";
+            dropdownDiv.appendChild(item2);
+
+            var item3 = document.createElement('a');
+            item3.className = "dropdown-item";
+            item3.id = "yellowColor";
+            item3.href = "#";
+            item3.innerText = "Yellow";
+            dropdownDiv.appendChild(item3);
+          }
+      });
+    }
+}
+
+function addSelectColorButtonFunctionality(){
+  $('#blueColor').click( function() {
+    console.log("clicked the color blue! ");
+    setColor("blue");
+    saveStorage(this.parentNode.parentNode.id, "blue"); // saving the new color
+  });
+
+  $('#redColor').click( function() {
+    console.log("clicked the color red!");
+    setColor("red");
+    saveStorage(this.parentNode.parentNode.id, "red"); // saving the new
+  });
+
+  $('#yellowColor').click( function() {
+    console.log("clicked the color yellow!");
+    setColor("yellow");
+    saveStorage(this.parentNode.parentNode.id, "yellow"); // saving the new
+  });
+}
+
+/*
+Display Map Button
+ */
+function addDisplayMapButtonFunctionality(){
+  // on click of custom button, remove the selection from all other buttons and add selction to this one
+  var list= document.getElementsByClassName("section-tab-bar-tab");
+  $('#CustomMapButton').click(function() {
+      for (var i = 0; i < list.length; i++) {
+          var myelem = list[i];
+          $(myelem).removeClass('section-tab-bar-tab-selected');
+          $(myelem).addClass('section-tab-bar-tab-unselected');
+      }
+      $(this).removeClass('section-tab-bar-tab-unselected');
+      $(this).addClass('section-tab-bar-tab-selected');
+  });
+  // on click of the other buttons, remove the selection from custom button
+  for (var i = 0; i < list.length; i++) {
+      var myelem = list[i];
+      if (myelem.id != "CustomMapButton") {
+        $(myelem).click(function() {
+          $('#CustomMapButton').removeClass('section-tab-bar-tab-selected');
+          $('#CustomMapButton').addClass('section-tab-bar-tab-unselected');
+        });
+      }
+    }
+  }
+
+function addDisplayMapButton(){
+  var existingElement = document.getElementById("CustomMapButton");
+  var allButtons = document.querySelector('div.section-tab-bar');
+  if ( allButtons!=null & existingElement==null) {
+    var z = document.createElement('button');
+    z.id = "CustomMapButton";
+    z.className = "section-tab-bar-tab ripple-container section-tab-bar-tab-unselected";
+    z.innerText = "Display Map";
+    allButtons.appendChild(z);
+  }
+}
+
+/*
+google maps
+*/
 function googleMapsAPI(apikey){
   console.log("loading api");
 
@@ -69,153 +255,4 @@ function googleMapsAPI(apikey){
   script.async = false;
   document.body.appendChild(script);
   console.log("appended script tag with api");
-}
-
-function setListColor(mylist, color){
-  mylist.forEach( function(currentValue) {
-    element = currentValue;
-    textElement = element.querySelector("div.section-list-item-text");
-    icon = element.querySelector("div.section-common-icon");
-
-    if (textElement.textContent.includes('cafe')) {
-      console.log("text contains 'cafe'");
-      icon.style.backgroundColor = color;
-    }
-  });
-}
-
-function accessListElements() {
-  var singleElement = document.querySelectorAll("div.section-common-icon");
-  var savedPlacesList = document.querySelectorAll("button.section-list-item-content.section-list-item-button");
-  return savedPlacesList;
-}
-
-function saveStorage(key, value) {
-  var obj = {}
-  obj[key]=value;
-  chrome.storage.sync.set(obj, function(){
-    console.log("saving ", key);
-  });
-
-  // make sure its saved
-  chrome.storage.sync.get(key, function(result) {
-    var value = result[key];
-  });
-}
-
-//////////////////////// html additions ////////////////////
-
-////// select color button
-
-function addSelectColorButton() {
-  var parent = document.getElementById("context-menu");
-  var existingElement = document.getElementById("newOutterDiv");
-  if (parent != null && existingElement == null) {
-    // outter most div
-    console.log("adding select color button");
-    var d = document.createElement('div');
-    d.id = "newOutterDiv";
-    d.className = "context-menu-entry kd-menulistitem dropright show";
-    parent.appendChild(d);
-
-    // Select Color Button
-    var justMadeDiv = document.getElementById("newOutterDiv");
-    var b = document.createElement('button');
-    b.id = "selectColorButton";
-    b.className = "context-menu-entry-text dropdown-toggle";
-    b.setAttribute("data-toggle", "dropdown");
-    b.innerText = "Select Color";
-    justMadeDiv.appendChild(b);
-
-    // the dropdown content
-    var dropdownDiv = document.createElement('div');
-    dropdownDiv.className="dropdown-menu";
-    dropdownDiv.setAttribute("aria-labelledby","dropdownMenuLink");
-    justMadeDiv.appendChild(dropdownDiv);
-
-    var item1 = document.createElement('a');
-    item1.className = "dropdown-item";
-    item1.id = "redColor";
-    item1.href = "#";
-    item1.innerText = "Red";
-    dropdownDiv.appendChild(item1);
-
-    var item2 = document.createElement('a');
-    item2.className = "dropdown-item";
-    item2.id = "blueColor";
-    item2.href = "#";
-    item2.innerText = "Blue";
-    dropdownDiv.appendChild(item2);
-
-    var item2 = document.createElement('a');
-    item2.className = "dropdown-item";
-    item2.id = "yellowColor";
-    item2.href = "#";
-    item2.innerText = "Yellow";
-    dropdownDiv.appendChild(item2);
-  }
-}
-
-function addSelectColorButtonFunctionality(){
-  $('#blueColor').click( function() {
-    console.log("clicked the color blue!");
-    var mylist = accessListElements();
-    setListColor(mylist, "blue");
-    saveStorage(colorkey, "blue"); // saving the new
-  });
-
-  $('#redColor').click( function() {
-    console.log("clicked the color red!");
-    var mylist = accessListElements();
-    setListColor(mylist, "red");
-    saveStorage(colorkey, "red"); // saving the new
-  });
-
-  $('#yellowColor').click( function() {
-    console.log("clicked the color yellow!");
-    var mylist = accessListElements();
-    setListColor(mylist, "yellow");
-    saveStorage(colorkey, "yellow"); // saving the new
-  });
-}
-
-////// DisplayMap Button
-
-function addDisplayMapButtonFunctionality(){
-  // on click of custom button,
-  // remove the selection from all other buttons and add selction to this one
-  var list= document.getElementsByClassName("section-tab-bar-tab");
-  $('#CustomMapButton').click(function() {
-      for (var i = 0; i < list.length; i++) {
-          var myelem = list[i];
-          $(myelem).removeClass('section-tab-bar-tab-selected');
-          $(myelem).addClass('section-tab-bar-tab-unselected');
-      }
-      $(this).removeClass('section-tab-bar-tab-unselected');
-      $(this).addClass('section-tab-bar-tab-selected');
-  });
-  // on click of the other buttons,
-  // remove the selection from custom button
-  for (var i = 0; i < list.length; i++) {
-      var myelem = list[i];
-      if (myelem.id != "CustomMapButton") {
-        $(myelem).click(function() {
-          $('#CustomMapButton').removeClass('section-tab-bar-tab-selected');
-          $('#CustomMapButton').addClass('section-tab-bar-tab-unselected');
-        });
-      }
-    }
-  }
-
-function addDisplayMapButton(){
-  var existingElement = document.getElementById("CustomMapButton");
-  var allButtons = document.querySelector('div.section-tab-bar');
-  if ( allButtons!=null & existingElement==null) {
-    var z = document.createElement('button');
-    z.id = "CustomMapButton";
-    z.className = "section-tab-bar-tab ripple-container section-tab-bar-tab-unselected";
-    z.innerText = "Display Map";
-    allButtons.appendChild(z);
-  }
-
 }
